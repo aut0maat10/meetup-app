2d759b74189460ac6d77163706ce246b2ba99293
Robin Pahlman <robin.pahlman@gmail.com>
2017-11-10 10:28:56 -0500

    add helpers class, add signup view and route, add logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,31 @@ class UsersController < ApplicationController
     get '/signup' do
         erb :'users/signup' 
     end 
+
+    post '/signup' do
+        #binding.pry 
+        if !params[:name].empty? && !params[:email].empty? && !params[:password].empty? 
+            @user = User.new(params)
+            @user.save
+            session[:id] = @user.id
+            redirect '/meetups'
+        else
+            redirect '/signup'
+        end 
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+    
+    get '/meetups' do
+        #binding.pry 
+        if session[:id]
+            @user = Helpers.current_user(session)
+            erb :'/meetups/index'
+        else
+            redirect :'/'
+        end
+    end 
 end 
