a19a805bb876de8fce2e782ac398ac46495118df
Robin Pahlman <robin.pahlman@gmail.com>
2017-11-22 16:27:27 -0500

    add edits to meetups_controller, fix views and bootstrap issue

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 module Helpers
 
     def current_user #=> user instance || nil
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        @current_user ||= User.find_by(id: session[:id]) if session[:id]
     end
 
     def is_logged_in?
